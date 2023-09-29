fa3dfd55af86fd3d59392a8bc615b69ea9e140c1
felipesanderp <felipe.panisson@hotmail.com>
2023-09-29 11:54:25 -0300

    get user meals and create meal route

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -2,13 +2,53 @@ import { FastifyInstance } from 'fastify'
 import { randomUUID } from 'node:crypto'
 import { z } from 'zod'
 import { knex } from '../database'
+import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
 
 export async function mealsRoutes(app: FastifyInstance) {
-  app.get('/', async () => {
-    const meals = await knex('meals').select()
+  app.get('/', { preHandler: [checkSessionIdExists] }, async (request) => {
+    const { sessionId } = request.cookies
+
+    const [user] = await knex('users')
+      .where('session_id', sessionId)
+      .select('id')
+
+    const meals = await knex('meals').where('user_id', user.id).select()
 
     return { meals }
   })
 
-  app.post('/', async () => {})
+  app.post(
+    '/',
+    { preHandler: [checkSessionIdExists] },
+    async (request, response) => {
+      const { sessionId } = request.cookies
+
+      const [user] = await knex('users')
+        .where('session_id', sessionId)
+        .select('id')
+
+      const createMealBodySchema = z.object({
+        name: z.string(),
+        description: z.string(),
+        mealDate: z.string(),
+        mealHour: z.string(),
+        isOnTheDiet: z.boolean(),
+      })
+
+      const { name, description, mealDate, mealHour, isOnTheDiet } =
+        createMealBodySchema.parse(request.body)
+
+      await knex('meals').insert({
+        id: randomUUID(),
+        name,
+        user_id: user.id,
+        description,
+        mealDate,
+        mealHour,
+        isOnTheDiet,
+      })
+
+      return response.status(201).send()
+    },
+  )
 }
