73a898d0354fe1478a35ff8f0514d0d7cb25fe9e
felipesanderp <felipe.panisson@hotmail.com>
2023-09-29 16:11:14 -0300

    update profile route

--- src/routes/profile.ts
+++ src/routes/profile.ts
@@ -1,7 +1,8 @@
 import { FastifyInstance } from 'fastify'
-// import { z } from 'zod'
+import { z } from 'zod'
 import { knex } from '../database'
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists'
+import { hash } from 'bcrypt'
 
 export async function profileRoutes(app: FastifyInstance) {
   app.get(
@@ -22,6 +23,38 @@ export async function profileRoutes(app: FastifyInstance) {
     },
   )
 
-  // TODO update profile
-  // app.put('/', { preHandler: [checkSessionIdExists] }, async () => {})
+  app.put(
+    '/',
+    { preHandler: [checkSessionIdExists] },
+    async (request, response) => {
+      const { sessionId } = request.cookies
+
+      const editProfileBodySchema = z.object({
+        name: z.string().optional(),
+        email: z.string().email().optional(),
+        password: z.string().optional(),
+      })
+
+      const { name, email, password } = editProfileBodySchema.parse(
+        request.body,
+      )
+
+      if (password) {
+        const passwordHash = await hash(password, 10)
+
+        await knex('users').where('session_id', sessionId).update({
+          password: passwordHash,
+        })
+
+        return response.status(202).send()
+      }
+
+      await knex('users').where('session_id', sessionId).update({
+        name,
+        email,
+      })
+
+      return response.status(202).send()
+    },
+  )
 }
