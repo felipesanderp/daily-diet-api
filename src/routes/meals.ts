590efc0ff10c374005130d89a1d948410ec67b7f
felipesanderp <felipe.panisson@hotmail.com>
2023-09-29 12:19:51 -0300

    delet meal and get meals summary

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -51,4 +51,82 @@ export async function mealsRoutes(app: FastifyInstance) {
       return response.status(201).send()
     },
   )
+
+  app.delete(
+    '/:id',
+    { preHandler: [checkSessionIdExists] },
+    async (request, response) => {
+      const deleteMealParamsSchema = z.object({
+        id: z.string().uuid(),
+      })
+
+      const params = deleteMealParamsSchema.parse(request.params)
+
+      const { sessionId } = request.cookies
+
+      const [user] = await knex('users')
+        .where('session_id', sessionId)
+        .select('id')
+
+      const meal = await knex('meals')
+        .where('id', params.id)
+        .andWhere('user_id', user.id)
+        .first()
+        .delete()
+
+      if (!meal) {
+        return response.status(401).send({
+          error: 'Unauthorized',
+        })
+      }
+
+      return response.status(202).send('Meal deleted')
+    },
+  )
+
+  app.get(
+    '/summary',
+    { preHandler: [checkSessionIdExists] },
+    async (request) => {
+      const { sessionId } = request.cookies
+
+      const [user] = await knex('users')
+        .where('session_id', sessionId)
+        .select('id')
+
+      const [count] = await knex('meals')
+        .count('id', {
+          as: 'Total de refeições registradas',
+        })
+        .where('user_id', user.id)
+
+      const refOnDiet = await knex('meals')
+        .count('id', { as: 'onDiet' })
+        .where('isOnTheDiet', true)
+        .andWhere('user_id', user.id)
+
+      const refOutsideDiet = await knex('meals')
+        .count('id', { as: 'outsideDiet' })
+        .where('isOnTheDiet', false)
+        .andWhere('user_id', user.id)
+
+      const summary = {
+        'Total de refeições registradas': parseInt(
+          JSON.parse(JSON.stringify(count))['Total de refeições registradas'],
+        ),
+
+        'Total de refeições dentro da dieta': parseInt(
+          JSON.parse(JSON.stringify(refOnDiet))[0].onDiet,
+        ),
+
+        'Total de refeições fora da dieta': parseInt(
+          JSON.parse(JSON.stringify(refOutsideDiet))[0].outsideDiet,
+        ),
+      }
+
+      return {
+        summary,
+      }
+    },
+  )
 }
